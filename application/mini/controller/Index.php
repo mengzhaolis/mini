bb45e28adfcbe8394ddd47e42e12d41495f4d573
mengzhaolis <1912882425@qq.com>
2017-11-29 15:05:15 +0800

    change login

--- application/mini/controller/Index.php
+++ application/mini/controller/Index.php
@@ -22,56 +22,64 @@ Class Index
     //正常页验证登陆
     public function l(Request $request)
     {
-        $data = $request->post();
-        // var_dump($data);die;
-        if(!empty($data['shop']))
+         $data = $request->post();
+         Session::set('url',$data['test']);
+        if(empty(Session::get('username')))
         {
+            return false;
+        }else
+        {
+            $data = $request->post();
+            // var_dump($data);die;
             if(!empty($data['test']))
             {
                 Session::set('url',$data['test']);
+                return true;
+            }else
+            {
+                return false;
             }
-            return 1;
         }
-        
     }
     public function login(Request $request)
     {
-           return view('login');
-        
+        return view('login');      
     }
     //执行登陆功能
     public function login_in(Request $request)
     {
-        $data = $request->post();
+            $data = $request->post();
         // var_dump($data);die;
         
-        $username = Db::table('user')->where('username','=',$data['Username'])->find();
-        if(empty($username))
-        {
-            return '<script>alert("用户名不存在");window.location.href="login.html"</script>';
-            
-        }else if($username['password'] == $data['Password'])
-        {
-            $user = $username['username'];
-            // $this->useres = $user;
-            $id = $username['id'];
-            Session::set("$id",$username['id']);
-            Session::set("$user",$username['username']);
-            // var_dump(Session::get("$user"));die;
-            if(!empty(Session::get($username['id']) || !empty($username['username'])))
+            $username = Db::table('user')->where('username','=',$data['Username'])->find();
+            if(empty($username))
             {
-                $url = Session::get('url');
+                return '<script>alert("用户名不存在");window.location.href="login.html"</script>';
+                
+            }else if($username['password'] == $data['Password'])
+            {
+                $user = $username['username'];
+                // $this->useres = $user;
+                $id = $username['id'];
+                Session::set("id",$username['id']);
+                Session::set("username",$username['username']);
+                // var_dump(Session::get("$user"));die;
+                if(!empty(Session::get('id') || !empty('username')))
+                {
+                    $url = Session::get('url');
 
-                // var_dump($url);die;
-                echo '<script>alert("登陆成功！！！");</script>';
-                echo '<script language="javascript">location.href="' . $url . '"; </script>';
+                    // var_dump($url);die;
+                    echo '<script>alert("登陆成功！！！");</script>';
+                    echo '<script language="javascript">location.href="' . $url . '"; </script>';
+                }
+                
+                // var_dump(Session::get('username'));die;
+            }else
+            {
+                return '<script>alert("密码错误！！！");window.location.href="login.html"</script>';
             }
-            
-            // var_dump(Session::get('username'));die;
-        }else
-        {
-            return '<script>alert("密码错误！！！");window.location.href="login.html"</script>';
-        }
+        
+        
     }
     //注册页面展示
     public function zhuce()
